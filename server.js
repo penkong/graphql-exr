7d53c1611dcda083ebcbf20c7df2619d127a17c1
mazdak <nazemi.works@gmail.com>
2019-09-02 11:45:11 -0700

    on more comments through learning

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const express = require('express');
 const expressGraphQL = require('express-graphql');
 const schema = require('./schema/schema');
 const app = express();
+// apollo and relay bring query from client to express then it go
+// to graphql and then out source api(json server) or db(local)
+// request (query) have 3 fields in headers in console can find it
+// operationName, query (all client like graphiql speak same lang on queries), variables.
+// react vue speak with graphql client and it will send query to back end
+// graphql client do job of graphiql  in real world
+// lokka, apollo client, relay. clients give us caching ability.
+// graphqlexress is reference implementation.
+// apollo server is same but different in implementation.
 app.use('/graphql', expressGraphQL({
   schema ,
   graphiql: true
