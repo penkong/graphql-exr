4abfc05b91ed31ab05c93872efac3e612380619b
mazdak <nazemi.works@gmail.com>
2019-09-03 08:42:47 -0700

    2 th app lyrical

---
+++ lyrical/server/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const models = require('./models');
+const expressGraphQL = require('express-graphql');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const schema = require('./schema/schema');
+
+const app = express();
+
+// Replace with your mongoLab URI
+const MONGO_URI = 'mongodb://mkz:mkz123@cluster0-shard-00-00-jeaya.mongodb.net:27017,cluster0-shard-00-01-jeaya.mongodb.net:27017,cluster0-shard-00-02-jeaya.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
+if (!MONGO_URI) {
+  throw new Error('You must provide a MongoLab URI');
+}
+
+mongoose.Promise = global.Promise;
+mongoose.connect(MONGO_URI);
+mongoose.connection
+    .once('open', () => console.log('Connected to MongoLab instance.'))
+    .on('error', error => console.log('Error connecting to MongoLab:', error));
+
+app.use(bodyParser.json());
+app.use('/graphql', expressGraphQL({
+  schema,
+  graphiql: true
+}));
+
+const webpackMiddleware = require('webpack-dev-middleware');
+const webpack = require('webpack');
+const webpackConfig = require('../webpack.config.js');
+app.use(webpackMiddleware(webpack(webpackConfig)));
+
+module.exports = app;
